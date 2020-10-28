071f461a0bc6705aa829d65420070afbd76a1976
Drake53 <49623303+Drake53@users.noreply.github.com>
2020-10-28 12:03:10 +0100

    Implement MapTriggers adapter.

---
+++ src/War3App.MapAdapter/Script/MapTriggersAdapter.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+using War3App.MapAdapter.Extensions;
+
+using War3Net.Build.Common;
+using War3Net.Build.Script;
+using War3Net.Common.Extensions;
+
+namespace War3App.MapAdapter.Script
+{
+    public sealed class MapTriggersAdapter : IMapFileAdapter
+    {
+        public string MapFileDescription => "Triggers";
+
+        public bool IsTextFile => false;
+
+        public bool CanAdaptFile(string s)
+        {
+            return string.Equals(s.GetFileExtension(), MapTriggers.FileName.GetFileExtension(), StringComparison.OrdinalIgnoreCase);
+        }
+
+        public bool CanAdaptFile(Stream stream)
+        {
+            if (stream.Length < 4)
+            {
+                return false;
+            }
+
+            using var reader = new BinaryReader(stream, Encoding.UTF8, true);
+            int header;
+            try
+            {
+                header = reader.ReadInt32();
+                stream.Position = 0;
+            }
+            catch (ArgumentException)
+            {
+                stream.Position = 0;
+                return false;
+            }
+
+            return header == "WTG!".FromRawcode();
+        }
+
+        public AdaptResult AdaptFile(Stream stream, GamePatch targetPatch, GamePatch originPatch)
+        {
+            try
+            {
+                var mapTriggers = MapTriggers.Parse(stream);
+                if (mapTriggers.GetMinimumPatch() <= targetPatch)
+                {
+                    return new AdaptResult
+                    {
+                        Status = MapFileStatus.Compatible,
+                    };
+                }
+
+                try
+                {
+                    if (mapTriggers.TryDowngrade(targetPatch))
+                    {
+                        var newMapTriggersFileStream = new MemoryStream();
+                        mapTriggers.SerializeTo(newMapTriggersFileStream, true);
+
+                        return new AdaptResult
+                        {
+                            Status = MapFileStatus.Adapted,
+                            AdaptedFileStream = newMapTriggersFileStream,
+                        };
+                    }
+                    else
+                    {
+                        return new AdaptResult
+                        {
+                            Status = MapFileStatus.Unadaptable,
+                        };
+                    }
+                }
+                catch
+                {
+                    return new AdaptResult
+                    {
+                        Status = MapFileStatus.AdapterError,
+                    };
+                }
+            }
+            catch (Exception e)
+            {
+                return new AdaptResult
+                {
+                    Status = MapFileStatus.ParseError,
+                    Diagnostics = new[] { e.Message },
+                };
+            }
+        }
+    }
+}
---
+++ src/War3App.MapAdapter/Script/MapTriggersExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+using War3Net.Build.Common;
+using War3Net.Build.Script;
+
+namespace War3App.MapAdapter.Script
+{
+    public static class MapTriggersExtensions
+    {
+        public static bool TryDowngrade(this MapTriggers mapTriggers, GamePatch targetPatch)
+        {
+            try
+            {
+                while (mapTriggers.GetMinimumPatch() > targetPatch)
+                {
+                    mapTriggers.DowngradeOnce();
+                }
+
+                return true;
+            }
+            catch (NotSupportedException)
+            {
+                return false;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static void DowngradeOnce(this MapTriggers mapTriggers)
+        {
+            if (mapTriggers.UseNewFormat)
+            {
+                mapTriggers.UseNewFormat = false;
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        public static GamePatch GetMinimumPatch(this MapTriggers mapTriggers)
+        {
+            return mapTriggers.UseNewFormat
+                ? GamePatch.v1_31_0
+                : mapTriggers.FormatVersion == MapTriggersFormatVersion.Tft
+                    ? GamePatch.v1_07
+                    : GamePatch.v1_00;
+        }
+    }
+}
