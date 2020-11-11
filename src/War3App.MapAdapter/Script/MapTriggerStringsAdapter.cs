78e060108b5aca3e5ce58ec7bcc6554fb475fab9
Drake53 <49623303+Drake53@users.noreply.github.com>
2020-11-11 18:10:41 +0100

    Add war3map.wts adapter.

---
+++ src/War3App.MapAdapter/Script/MapTriggerStringsAdapter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+using War3App.MapAdapter.Extensions;
+
+using War3Net.Build.Common;
+using War3Net.Build.Script;
+
+namespace War3App.MapAdapter.Script
+{
+    public sealed class MapTriggerStringsAdapter : IMapFileAdapter
+    {
+        public string MapFileDescription => "Trigger Strings";
+
+        public bool IsTextFile => true;
+
+        public bool CanAdaptFile(string s)
+        {
+            return string.Equals(s.GetFileExtension(), MapTriggerStrings.FileName.GetFileExtension(), StringComparison.OrdinalIgnoreCase);
+        }
+
+        public bool CanAdaptFile(Stream stream)
+        {
+            return false;
+        }
+
+        public AdaptResult AdaptFile(Stream stream, GamePatch targetPatch, GamePatch originPatch)
+        {
+            try
+            {
+                return new AdaptResult
+                {
+                    Status = MapFileStatus.Compatible,
+                };
+            }
+            catch (NotSupportedException)
+            {
+                return new AdaptResult
+                {
+                    Status = MapFileStatus.Unadaptable,
+                };
+            }
+            catch (Exception e)
+            {
+                return new AdaptResult
+                {
+                    Status = MapFileStatus.ParseError,
+                    Diagnostics = new[] { e.Message },
+                };
+            }
+        }
+    }
+}
