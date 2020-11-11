adde85a7b64b0d3e1bbdc8575475f037124076a8
Drake53 <49623303+Drake53@users.noreply.github.com>
2020-11-11 18:09:25 +0100

    Add MapCameras adapter.

---
+++ src/War3App.MapAdapter/Environment/MapCamerasAdapter.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+
+using War3App.MapAdapter.Extensions;
+
+using War3Net.Build.Common;
+using War3Net.Build.Environment;
+
+namespace War3App.MapAdapter.Environment
+{
+    public sealed class MapCamerasAdapter : IMapFileAdapter
+    {
+        public string MapFileDescription => "Cameras";
+
+        public bool IsTextFile => false;
+
+        public bool CanAdaptFile(string s)
+        {
+            return string.Equals(s.GetFileExtension(), MapCameras.FileName.GetFileExtension(), StringComparison.OrdinalIgnoreCase);
+        }
+
+        public bool CanAdaptFile(Stream stream)
+        {
+            return false;
+        }
+
+        public AdaptResult AdaptFile(Stream stream, GamePatch targetPatch, GamePatch originPatch)
+        {
+            try
+            {
+                var mapCameras = MapCameras.Parse(stream);
+                if (mapCameras.GetMinimumPatch() <= targetPatch)
+                {
+                    return new AdaptResult
+                    {
+                        Status = MapFileStatus.Compatible,
+                    };
+                }
+
+                try
+                {
+                    if (mapCameras.TryDowngrade(targetPatch))
+                    {
+                        var newMapCamerasFileStream = new MemoryStream();
+                        mapCameras.SerializeTo(newMapCamerasFileStream, true);
+
+                        return new AdaptResult
+                        {
+                            Status = MapFileStatus.Adapted,
+                            AdaptedFileStream = newMapCamerasFileStream,
+                        };
+                    }
+                    else
+                    {
+                        return new AdaptResult
+                        {
+                            Status = MapFileStatus.Unadaptable,
+                        };
+                    }
+                }
+                catch
+                {
+                    return new AdaptResult
+                    {
+                        Status = MapFileStatus.AdapterError,
+                    };
+                }
+            }
+            catch (NotSupportedException)
+            {
+                return new AdaptResult
+                {
+                    Status = MapFileStatus.Unadaptable,
+                };
+            }
+            catch (Exception e)
+            {
+                return new AdaptResult
+                {
+                    Status = MapFileStatus.ParseError,
+                    Diagnostics = new[] { e.Message },
+                };
+            }
+        }
+    }
+}
---
+++ src/War3App.MapAdapter/Environment/MapCamerasExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using War3Net.Build.Common;
+using War3Net.Build.Environment;
+
+namespace War3App.MapAdapter.Environment
+{
+    public static class MapCamerasExtensions
+    {
+        public static bool TryDowngrade(this MapCameras mapCameras, GamePatch targetPatch)
+        {
+            try
+            {
+                while (mapCameras.GetMinimumPatch() > targetPatch)
+                {
+                    mapCameras.DowngradeOnce();
+                }
+
+                return true;
+            }
+            catch (NotSupportedException)
+            {
+                return false;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static void DowngradeOnce(this MapCameras mapCameras)
+        {
+            if (mapCameras.UseNewFormat)
+            {
+                mapCameras.UseNewFormat = false;
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        public static GamePatch GetMinimumPatch(this MapCameras mapCameras)
+        {
+            return mapCameras.UseNewFormat ? GamePatch.v1_31_0 : GamePatch.v1_00;
+        }
+    }
+}
