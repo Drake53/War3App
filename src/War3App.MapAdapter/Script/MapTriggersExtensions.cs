10cee7204f30325627439c0199a4f6fce42922f2
Drake53 <49623303+Drake53@users.noreply.github.com>
2024-11-18 21:46:56 +0100

    Fix adapting war3map.wtg file to pre-1.31 format.

--- src/War3App.MapAdapter/Script/MapTriggersExtensions.cs
+++ src/War3App.MapAdapter/Script/MapTriggersExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 using War3Net.Build.Common;
 using War3Net.Build.Script;
@@ -33,6 +35,10 @@ namespace War3App.MapAdapter.Script
             if (mapTriggers.SubVersion.HasValue)
             {
                 mapTriggers.SubVersion = null;
+                mapTriggers.TriggerItemCounts.Clear();
+
+                mapTriggers.OverrideIds();
+                mapTriggers.UpdateNestedTriggerCategoryNames();
             }
             else
             {
@@ -47,6 +53,58 @@ namespace War3App.MapAdapter.Script
                 : mapTriggers.FormatVersion == MapTriggersFormatVersion.v7
                     ? GamePatch.v1_07
                     : GamePatch.v1_00;
+        }
+
+        public static void OverrideIds(this MapTriggers mapTriggers)
+        {
+            var idRemapping = new Dictionary<int, int>();
+
+            for (var i = 0; i < mapTriggers.TriggerItems.Count; i++)
+            {
+                var triggerItem = mapTriggers.TriggerItems[i];
+
+                idRemapping[triggerItem.Id] = i;
+                triggerItem.Id = i;
+
+                if (idRemapping.TryGetValue(triggerItem.ParentId, out var newParentId))
+                {
+                    triggerItem.ParentId = newParentId;
+                }
+            }
+        }
+
+        public static void UpdateNestedTriggerCategoryNames(this MapTriggers mapTriggers)
+        {
+            var triggerCategories = mapTriggers.TriggerItems
+                .Where(triggerItem => triggerItem is TriggerCategoryDefinition triggerCategory && triggerItem.Type != TriggerItemType.RootCategory)
+                .ToDictionary(
+                    triggerItem => triggerItem.Id,
+                    triggerItem => (TriggerCategoryDefinition)triggerItem);
+
+            var triggerCategoryPaths = new Dictionary<int, string>();
+
+            IEnumerable<string> GetTriggerCategoryNames(TriggerCategoryDefinition triggerCategory)
+            {
+                if (triggerCategories.TryGetValue(triggerCategory.ParentId, out var parentTriggerCategory))
+                {
+                    return GetTriggerCategoryNames(parentTriggerCategory).Append(triggerCategory.Name);
+                }
+
+                return Enumerable.Repeat(triggerCategory.Name, 1);
+            }
+
+            foreach (var triggerCategory in triggerCategories)
+            {
+                triggerCategoryPaths.Add(triggerCategory.Key, string.Join("/", GetTriggerCategoryNames(triggerCategory.Value)));
+            }
+
+            foreach (var triggerCategory in triggerCategories)
+            {
+                if (triggerCategoryPaths.TryGetValue(triggerCategory.Key, out var triggerCategoryPath))
+                {
+                    triggerCategory.Value.Name = triggerCategoryPath;
+                }
+            }
         }
     }
 }
