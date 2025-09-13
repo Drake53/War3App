527d9874b2a2959a70d0852d3dff1550d9181155
Drake53 <49623303+Drake53@users.noreply.github.com>
2025-09-13 10:10:27 +0200

    Fix exception text.

--- src/War3App.MapAdapter/Constants/ExceptionText.cs
+++ src/War3App.MapAdapter/Constants/ExceptionText.cs
@@ -4,6 +4,6 @@ namespace War3App.MapAdapter.Constants
     {
         public const string OpenArchive = "Background task 'open archive' did not complete succesfully.";
         public const string SaveArchive = "Background task 'save archive' did not complete succesfully.";
-        public const string ChildrenRemoved = "Parent tag should have been removed since all child tags are removed, but was {0}.";
+        public const string ChildrenRemoved = "Parent file should have been removed since all child files are removed, but was {0}.";
     }
 }
